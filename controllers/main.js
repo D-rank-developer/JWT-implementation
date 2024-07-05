0c5476241f2e88c8d606e488baa6ec476ea526f5
Dumanyie Chamberlain <93911676+Dumanyie-18CJ024115@users.noreply.github.com>
2024-07-05 10:17:12 +0100

    the ssecond commit

---
+++ controllers/main.js
@@ -0,0 +1,31 @@
+
+const jwt= require('jsonwebtoken');
+const CustomAPIError= require('../errors/custom-error')
+
+const login= async (req,res)=>{
+    const {username, password}= req.body
+    if(!username || ! password){
+        throw new CustomAPIError("please provide email and password", 400)
+
+    }
+
+    const id= new Date().getDate()// for the demo database
+// trying to get the jwt token
+
+    const token = jwt.sign({id,username}, process.env.JWT_SECRETS,{expiresIn:'30d'})//providing a payload and jwt secret string, do not send confidential information in here
+    res.status(200).json({msg:'user created', token})// setting authorization header and with the use of a bearer schema
+    // signing a token and giving it to the front end request
+    //emphazizing on securing resource access on the server
+
+    res.send('Fake Login/Register/Signup Route')
+    }
+    
+    const dashboard = async(req,res)=>{
+        console.log(req.headers);
+        const luckyNumber= Math.floor(Math.random()*100)
+        res.status(200).json({msg:`Hello, im chamberlain`, secret:` Here is your authorized data, your lucky number is ${luckyNumber}`})
+    }
+    
+    module.exports ={
+        login, dashboard
+    }
